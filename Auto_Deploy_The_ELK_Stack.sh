bf103c2147e1dd786b9267fc9aa27b15dd5b06bc
Sean Lawlor <108025291+Lawlorsj01@users.noreply.github.com>
2023-04-25 11:10:37 -0400

    Update Auto_Deploy_The_ELK_Stack.sh

--- Auto_Deploy_The_ELK_Stack.sh
+++ Auto_Deploy_The_ELK_Stack.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
+echo "Removing Default configs..."
 sudo rm /etc/elasticsearch/elasticsearch.yml
-
 sudo rm /etc/kibana/kibana.yml
 
-sudo cp ./elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
+#Find Primary Interface using successfull query to 8.8.8.8
+interface_name=$(ip route get 8.8.8.8 | awk -- '{printf $5}')
+echo "Elk Stack using interface $interface_name"
+host_ip=$(ip addr show $interface_name | grep -oP 'inet \K[\d.]+')
+echo "ELK Stack to be configured on IP $host_ip"
 
+echo "Backing up and writing ELK Configs..."
+sed -i.bak "s|localhost|$host_ip|g" elasticsearch.yml
+sed -i.bak "s|localhost|$host_ip|g" kibana.yml
+
+echo "Moving generated configs to default install location"
+sudo cp ./elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
 sudo cp ./kibana.yml /etc/kibana/kibana.yml
 
+echo "Copying Certs"
 sudo mkdir /etc/kibana/certs
 sudo cp /etc/elasticsearch/certs/http_ca.crt /etc/kibana/certs/http_ca.crt
 
-sudo /usr/share/elasticsearch/bin/elasticsearch-setup-passwords interactive
-
+echo "Changing Ownership"
 sudo chown -R /usr/share/elasticsearch/
-
 sudo chown -R /etc/elasticsearch
-
 sudo systemctl restart elasticsearch.service
 
+echo "Setting Default Password"
 sudo /usr/share/elasticsearch/bin/elasticsearch-reset-password -b -u elastic -interactive <<EOF
 changeme
 changeme
